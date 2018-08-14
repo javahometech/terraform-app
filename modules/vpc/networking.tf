1fc2257fb93507ea6aba6d3f3110a2a4f10a308d
Hari Kammana <hari.kammana@gmail.com>
2018-08-14 23:09:51 +0530

    terraform modules

---
+++ modules/vpc/networking.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "${var.vpc_cidr}"
+  instance_tenancy = "${var.tenancy}"
+
+  tags {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${var.vpc_id}"
+  cidr_block = "${var.subnet_cidr}"
+
+  tags {
+    Name = "Main"
+  }
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
+
+output "subnet_id" {
+  value = "${aws_subnet.main.id}"
+}
---
+++ prod/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "my_vpc" {
+  source      = "../modules/vpc"
+  vpc_cidr    = "192.168.0.0/16"
+  tenancy     = "default"
+  vpc_id      = "${module.my_vpc.vpc_id}"
+  subnet_cidr = "192.168.1.0/24"
+}
+
+module "my_ec2" {
+  source        = "../modules/ec2"
+  ec2_count     = 1
+  ami_id        = "ami-5a8da735"
+  instance_type = "t2.micro"
+  subnet_id     = "${module.my_vpc.subnet_id}"
+}
